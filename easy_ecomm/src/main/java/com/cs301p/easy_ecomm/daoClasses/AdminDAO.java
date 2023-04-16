7e9c41821ae2715cd6c2dc5bdbc3134129270359
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-04-16 11:51:30 +0530

    AdminDAO completed

--- easy_ecomm/src/main/java/com/cs301p/easy_ecomm/daoClasses/AdminDAO.java
+++ easy_ecomm/src/main/java/com/cs301p/easy_ecomm/daoClasses/AdminDAO.java
@@ -1,10 +1,15 @@
 package com.cs301p.easy_ecomm.daoClasses;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 // import org.springframework.stereotype.Component;
+
+import com.cs301p.easy_ecomm.entityClasses.Admin;
+import com.cs301p.easy_ecomm.mappers.AdminMapper;
 
 // @Component
 public class AdminDAO {
@@ -33,5 +38,10 @@ public class AdminDAO {
     }
 
     // Now, write query functions.
-    
+    public Admin getAdminById(Admin admin) {
+        String sql = "SELECT * FROM admin WHERE id=" + admin.getId();
+        List<Admin> admins = this.jdbcTemplate.query(sql, new AdminMapper());
+
+        return admins.get(0);
+    }
 }
